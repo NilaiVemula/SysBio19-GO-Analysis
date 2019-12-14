8a5a2c8148512ecb9ae917e173a70e955fa7eb1b
Nilai Vemula <nilaivemula@gmail.com>
2019-12-14 15:27:36 -0600

    final push with functional enrichment and new data

--- scripts/functional_analysis.R
+++ scripts/functional_analysis.R
@@ -1,62 +1,26 @@
 #!/usr/bin/env Rscript
 
-require(optparse)
+library(tidyverse)
 
-setwd("../logs")
-print(getwd())
+source("scripts/FunctionalEnrichmentAnalysis.R")
 
-source("../scripts/04-functional_analysis/FunctionalEnrichmentAnalysis.R")
+genes_df <- read_csv("data/gene_list.csv")
 
-## parse command line args
-parseCommandLineArgs <- function() {
-    options <- list(
-                    make_option(c("-r", "--runName"),
-                                help="run name",
-                                type = "character")
-                    ) # end optionList definition
+output_folder <- "outputs/"
 
-    parse_args(OptionParser(option_list = options))
-}
+attractor_list <- unique(genes_df$Attractor_State)
 
-args <- parseCommandLineArgs()
-run_name = args$runName
-
-dir <- getwd()
-file <- paste("../results/post-processing/",run_name,"/fa_annotation/gene_expression_and_module_membership.txt",sep="")
-
-gene_expression_and_module_membership <- read.delim(file)
-dt <- data.frame(gene_expression_and_module_membership$Gene,gene_expression_and_module_membership$Module,gene_expression_and_module_membership$Metamodule)
-colnames(dt) <- c("ensemblid","module_name","metamodule")
-
-
-output_folder <- paste("../results/post-processing/", run_name, "/functional-enrichment/", sep="")
-dir.create(file.path(output_folder), showWarnings = FALSE)
-
-module_list <- unique(dt$module_name)
-
-for(key_module
-    in module_list){
-
-module_genes_df   <- dt[dt$module_name == key_module, ]
-
-f <- geneEnrichmentAnalysis(
-    output_folder = output_folder,
-    gene_list = module_genes_df,
-    title = paste0("module_", key_module)
-)
-}
-
-
-metamodule_list <- unique(dt$metamodule)
-
-for(key_metamodule
-    in metamodule_list){
-
-  metamodule_genes_df <- dt[dt$metamodule == key_metamodule, ]
+for(key_attractor
+    in attractor_list){
+  
+  attractor_genes_df <- genes_df[genes_df$Attractor_State == key_attractor, ]
+  
   f <- geneEnrichmentAnalysis(
-      output_folder = output_folder,
-      gene_list = metamodule_genes_df,
-      title = paste0("metamodule_", key_metamodule)
+    output_folder = output_folder,
+    gene_list = attractor_genes_df,
+    title = paste0("attractor_", key_attractor)
   )
-
 }
+
+
+
